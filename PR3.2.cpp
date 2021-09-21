357d101b6d2702aa2e9d99645146b832ee295851
ishtvankomka <you@example.com>
2021-09-21 21:06:14 +0300

    first master commit

---
+++ PR3.2.cpp
@@ -0,0 +1,39 @@
+// Lab_03_2.cpp
+// Комоній Іштван Іштванович
+// Лабораторна робота № 3.2
+// Розгалуження, задане формулою: функція з параметрами.
+// Варіант 11
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; 
+	double a; 
+	double b; 
+	double c; 
+	double F; 
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+	
+	if (x < 1 && c != 0)
+		F = a * x * x + (b / c);
+	if (x > 1.5 && c == 0)
+		F = (x - a) / ((x - c) * (x - c));
+	if (!(x < 1 && c != 0) && !(x > 1.5 && c == 0))
+		F = (x * x) / (c * c);
+	cout << endl;
+	cout << "1) F = " << F << endl;
+	
+	if (x < 1 && c != 0)
+		F = a * x * x + (b / c);
+	else
+		if (x > 1.5 && c == 0)
+			F = (x - a) / ((x - c) * (x - c));
+		else
+			F = (x * x) / (c * c);
+	cout << "2) F = " << F << endl;
+	cin.get();
+	return 0;
+}
